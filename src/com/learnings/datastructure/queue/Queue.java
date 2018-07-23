7fe203a28047fb4b2c597d55b95d01bd72bc7520
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2018-07-23 00:00:35 -0700

    Queue Implementation - Madanraj Venkatesan

--- src/com/learnings/datastructure/queue/Queue.java
+++ src/com/learnings/datastructure/queue/Queue.java
@@ -1,5 +1,9 @@
 package com.learnings.datastructure.queue;
 
+/**
+ * 
+ * @author Madanraj Venkatesan
+ */
 public class Queue {
 	
 	private int size;
@@ -7,7 +11,12 @@ public class Queue {
 	private int currentPushIndex;
 	private int currentPopIndex;
 
-	
+	/**
+	 * <p>
+	 * 		Queue Constructor
+	 * 
+	 * @param size
+	 */
 	public Queue(int size) {
 		this.size = size;
 		this.data = new Object[size];
@@ -15,6 +24,12 @@ public class Queue {
 		this.currentPopIndex = -1;
 	}
 	
+	/**
+	 * <p>
+	 * 		This method is to Add new elements to the Queue.
+	 * 
+	 * @param element - element.
+	 */
 	public void enqueue(Object element) {
 		if(isFull()) {
 			throw new QueueIndexOutOfBoundsException("The Queue is Full");
@@ -25,6 +40,12 @@ public class Queue {
 		}
 	}
 	
+	/**
+	 * <p>
+	 * 		This method is to get the existing elements from the Queue.
+	 * 
+	 * @return Object - Current Queue Item
+	 */
 	public Object dequeue() {
 		if(isEmpty()) {
 			throw new QueueIndexOutOfBoundsException("The Queue is Empty");
@@ -35,11 +56,24 @@ public class Queue {
 		}
 	}
 
+	/**
+	 * <p>
+	 * 		Returns whether the Queue is empty or not.
+	 * 
+	 * @return isEmpty
+	 */
 	public boolean isEmpty() {
 		return this.currentPopIndex == currentPushIndex;
 	}
 
+	/**
+	 * <p>
+	 * 		Returns whether the Queue is full or not.
+	 * 
+	 * @return isFull
+	 */
 	public boolean isFull() {
 		return this.currentPushIndex == size - 1;
 	}
 }
+
