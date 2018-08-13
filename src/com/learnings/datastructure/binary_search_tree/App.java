bc391106c8f230342613d99db01d4387b51081de
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2018-08-12 18:13:58 -0700

    Binary Search Tree Implementation - Madanraj Venkatesan

--- src/com/learnings/datastructure/binary_search_tree/App.java
+++ src/com/learnings/datastructure/binary_search_tree/App.java
@@ -10,8 +10,6 @@ public class App {
 		binaryTree.insert(30);
 		binaryTree.insert(90);
 		binaryTree.insert(70);
-		binaryTree.insert(70);
-		binaryTree.insert(90);
 		binaryTree.insert(100);
 		binaryTree.insert(1020);
 		binaryTree.insert(12);
@@ -19,58 +17,18 @@ public class App {
 		binaryTree.insert(8);
 		binaryTree.insert(2020);
 		binaryTree.insert(1500);
+		binaryTree.insert(0);
+		binaryTree.insert(0);
+		binaryTree.insert(999);
+		binaryTree.insert(9);
+		binaryTree.insert(25);
+		binaryTree.insert(26);
+		binaryTree.insert(75);
+		
+		System.out.println("Min Node: " + binaryTree.getMinNode().getData());
+		System.out.println("Max Node: " + binaryTree.getMaxNode().getData());
+		System.out.println("Searched Node: " + binaryTree.findNode(100).getData());
+		binaryTree.deleteNode(20);
 		System.out.println(binaryTree);
-		
-		System.out.print(getSpaces(15) + 1);
-		System.out.println();
-		System.out.print(getSpaces(14) + "/");
-		System.out.print(getSpaces(1) + "\\");
-		System.out.println();
-		System.out.print(getSpaces(13) + "/");
-		System.out.print(getSpaces(3) + "\\");
-		System.out.println();
-		System.out.print(getSpaces(12) + "/");
-		System.out.print(getSpaces(5) + "\\");
-		System.out.println();
-		System.out.print(getSpaces(11) + "/");
-		System.out.print(getSpaces(7) + "\\");
-		System.out.println();
-		System.out.print(getSpaces(10) + "/");
-		System.out.print(getSpaces(9) + "\\");
-		System.out.println();
-		System.out.print(getSpaces(9) + "/");
-		System.out.print(getSpaces(11) + "\\");
-		System.out.println();
-		
-		
-		System.out.print(getSpaces(8) + "2");
-		System.out.print(getSpaces(13) + "3");
-		System.out.println();
-		
-		System.out.print(getSpaces(7) + "/");
-		System.out.print(getSpaces(1) + "\\");
-		System.out.print(getSpaces(11) + "/");
-		System.out.print(getSpaces(1) + "\\");
-		System.out.println();
-		
-		System.out.print(getSpaces(6) + "/");
-		System.out.print(getSpaces(3) + "\\");
-		System.out.print(getSpaces(9) + "/");
-		System.out.print(getSpaces(3) + "\\");
-		System.out.println();
-		
-		System.out.print(getSpaces(5) + "4");
-		System.out.print(getSpaces(5) + "5");
-		System.out.print(getSpaces(7) + "6");
-		System.out.print(getSpaces(5) + "7");
-		System.out.println();
-		
-	}
-	private static String getSpaces(int count) {
-		StringBuilder spaceBuilder = new StringBuilder();
-		for(int i=0; i<count; i++) {
-			spaceBuilder.append(" ");
-		}
-		return spaceBuilder.toString();	
 	}
 }
--- src/com/learnings/datastructure/binary_search_tree/BinarySearchTree.java
+++ src/com/learnings/datastructure/binary_search_tree/BinarySearchTree.java
@@ -20,7 +20,7 @@ public class BinarySearchTree {
 	public void insertUsingLoop(Node currentNode, Node newNode) {
 
 		while(currentNode != null) {
-			if(newNode.getData() <= currentNode.getData()) {
+			if(newNode.getData() < currentNode.getData()) {
 				if(currentNode.getLeftNode() == null) {
 					currentNode.setLeftNode(newNode);
 					break;
@@ -29,7 +29,7 @@ public class BinarySearchTree {
 					currentNode = currentNode.getLeftNode();
 				}
 			}
-			else {
+			else if(newNode.getData() > currentNode.getData()) {
 				if(currentNode.getRightNode() == null) {
 					currentNode.setRightNode(newNode);
 					break;
@@ -38,12 +38,16 @@ public class BinarySearchTree {
 					currentNode = currentNode.getRightNode();
 				}
 			}
+			else {
+				System.out.println("Duplicate Node: " + newNode.getData());
+				break;
+			}
 		}
 	}
 
 	public void insertUsingRecursion(Node currentNode, Node newNode) {
 
-		if(newNode.getData() <= currentNode.getData()) {
+		if(newNode.getData() < currentNode.getData()) {
 			if(currentNode.getLeftNode() == null) {
 				currentNode.setLeftNode(newNode);
 			}
@@ -52,7 +56,7 @@ public class BinarySearchTree {
 				insertUsingRecursion(currentNode, newNode);
 			}
 		}
-		else {
+		else if(newNode.getData() > currentNode.getData()) {
 			if(currentNode.getRightNode() == null) {
 				currentNode.setRightNode(newNode);
 			}
@@ -61,6 +65,170 @@ public class BinarySearchTree {
 				insertUsingRecursion(currentNode, newNode);
 			}
 		}
+		else {
+			System.out.println("Duplicate Node: " + newNode.getData());
+		}
+	}
+
+	public Node getMaxNode() {
+
+		Node currentNode = rootNode;
+
+		while(currentNode != null) {
+			if(currentNode.getRightNode() != null)
+				currentNode = currentNode.getRightNode();
+			else
+				break;
+		}
+		return currentNode;
+	}
+	
+	public Node getMaxNode(Node currentNode) {
+
+		while(currentNode != null) {
+			if(currentNode.getRightNode() != null)
+				currentNode = currentNode.getRightNode();
+			else
+				break;
+		}
+		return currentNode;
+	}
+
+	public Node getMinNode() {
+
+		Node currentNode = rootNode;
+
+		while(currentNode != null) {
+			if(currentNode.getLeftNode() != null)
+				currentNode = currentNode.getLeftNode();
+			else
+				break;
+		}
+		return currentNode;
+	}
+	
+	public void deleteNode(int data) {
+	
+		// To Delete the Root Node
+		if(null != rootNode && data == rootNode.getData()) {
+			rootNode = null;
+		}
+		
+		Node currentNode, parentNode;
+		currentNode = parentNode = rootNode;
+		
+		// To Find the Node and Its Parent
+		while(currentNode != null) {
+			
+			if(data == currentNode.getData()) {
+				break;
+			}
+			else if(data < currentNode.getData()) {
+				parentNode = currentNode;
+				currentNode = currentNode.getLeftNode();
+			}
+			else {
+				parentNode = currentNode;
+				currentNode = currentNode.getRightNode();
+			}
+		}
+		
+		if(currentNode == null) {
+			System.out.println("Nothing to Delete.");
+			return;
+		}
+		else if(isLeafNode(currentNode)) {
+			parentNode.setLeftNode(null);
+			parentNode.setRightNode(null);
+		}
+		else if(isNodeHasOnlyLeftChildren(currentNode)) {
+			if(parentNode.getData() > currentNode.getData()) {
+				parentNode.setLeftNode(currentNode.getLeftNode());
+			}
+			else {
+				parentNode.setRightNode(currentNode.getLeftNode());
+			}
+		}
+		else if(isNodeHasOnlyRightChildren(currentNode)) {
+			if(parentNode.getData() > currentNode.getData()) {
+				parentNode.setLeftNode(currentNode.getRightNode());
+			}
+			else {
+				parentNode.setRightNode(currentNode.getRightNode());
+			}
+		}
+		else if(isNodeHasBothChildren(currentNode)) {
+			
+			//In-Order Successor Technique
+			Node inOrderSuccessorNode = currentNode.getRightNode();
+			Node inOrderSuccessorParentNode = currentNode.getRightNode();
+
+			while(inOrderSuccessorNode != null) {
+				if(inOrderSuccessorNode.getLeftNode() != null) {
+					inOrderSuccessorParentNode = inOrderSuccessorNode;
+					inOrderSuccessorNode = inOrderSuccessorNode.getLeftNode();
+				}
+				else
+					break;
+			}
+			
+			//Taking a backup of Right Node of the Node to be Deleted
+			Node rightNodeOfCurrentNode = currentNode.getRightNode();
+			
+			//Replacing the Current Node with In-Order Successor Node Details
+			currentNode.setData(inOrderSuccessorNode.getData());
+			if(inOrderSuccessorNode.getRightNode() != null) {
+				currentNode.setRightNode(inOrderSuccessorNode.getRightNode()); // Only right as there wont be any left node for sure
+			}
+			
+			//Find the Max of the In-Order Successor Right Node
+			Node inOrderSuccessorMaxNode = getMaxNode(inOrderSuccessorNode);
+			
+			//Assign the Right Node of your Current Node to the In-Order Successor Max Node
+			inOrderSuccessorMaxNode.setRightNode(rightNodeOfCurrentNode);
+			
+			//Delete the In-Order Successor Node
+			if(null != inOrderSuccessorParentNode.getLeftNode()) {
+				inOrderSuccessorParentNode.setLeftNode(null);
+			}
+			else {
+				inOrderSuccessorParentNode.setRightNode(null);
+			}
+		}
+	}
+	
+	private boolean isNodeHasOnlyRightChildren(Node currentNode) {
+		return (currentNode.getLeftNode() == null && currentNode.getRightNode() != null);
+	}
+
+	private boolean isNodeHasOnlyLeftChildren(Node currentNode) {
+		return (currentNode.getLeftNode() != null && currentNode.getRightNode() == null);
+	}
+
+	private boolean isNodeHasBothChildren(Node currentNode) {
+		return (currentNode.getLeftNode() != null && currentNode.getRightNode() != null);
+	}
+
+	private boolean isLeafNode(Node currentNode) {
+		return (currentNode.getLeftNode() == null && currentNode.getRightNode() == null);
+	}
+
+	public Node findNode(int data) {
+		
+		Node currentNode = rootNode;
+		
+		while(currentNode != null) {
+			if(data == currentNode.getData()) {
+				break;
+			}
+			else if(data < currentNode.getData()) {
+				currentNode = currentNode.getLeftNode();
+			}
+			else {
+				currentNode = currentNode.getRightNode();
+			}
+		}
+		return currentNode;
 	}
 
 	public boolean isBinaryTreeEmpty() {
