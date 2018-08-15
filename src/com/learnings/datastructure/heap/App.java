01472b35c6f560313c4b2a73397c320060f27a26
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2018-08-15 13:30:55 -0700

    Heap - Madanraj Venkatesan

---
+++ src/com/learnings/datastructure/heap/App.java
@@ -0,0 +1,5 @@
+package com.learnings.datastructure.heap;
+
+public class App {
+
+}
---
+++ src/com/learnings/datastructure/heap/MaxHeap.java
@@ -0,0 +1,21 @@
+package com.learnings.datastructure.heap;
+
+public class MaxHeap {
+	
+	private int [] data;
+	
+	private int maxSize;
+	
+	public MaxHeap(int maxSize) {
+		this.maxSize = maxSize;
+		this.data = new int[maxSize];
+	}
+	
+	public void insert(int value) {
+		
+	}
+	
+	public void delete(int value) {
+		
+	}
+}
