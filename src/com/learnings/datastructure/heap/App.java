f2d46ea890072d8cd6b14e23c5bf974ec15e0433
bn514d <bn514d@A5537442.nw.nos.boeing.com>
2018-08-15 18:23:14 -0700

    Heap Implementation in Progress - Madanraj Venkatesan

--- src/com/learnings/datastructure/heap/App.java
+++ src/com/learnings/datastructure/heap/App.java
@@ -1,5 +1,18 @@
 package com.learnings.datastructure.heap;
 
 public class App {
-
+	
+	public static void main(String[] args) {
+		MaxHeap heap = new MaxHeap(10);
+		heap.insert(10);
+		heap.insert(9);
+		heap.insert(8);
+		heap.insert(7);
+		heap.insert(6);
+		heap.insert(5);
+		heap.insert(4);
+		heap.insert(3);
+		heap.insert(20);
+		System.out.println(heap.viewHeap());
+	}
 }
--- src/com/learnings/datastructure/heap/MaxHeap.java
+++ src/com/learnings/datastructure/heap/MaxHeap.java
@@ -4,18 +4,72 @@ public class MaxHeap {
 	
 	private int [] data;
 	
-	private int maxSize;
+	private int size;
 	
-	public MaxHeap(int maxSize) {
-		this.maxSize = maxSize;
-		this.data = new int[maxSize];
+	private int currentIndex;
+	
+	private int capacity;
+	
+	public MaxHeap(int capacity) {
+		this.capacity = capacity;
+		this.data = new int[capacity];
+		this.size = 0;
+		this.currentIndex = -1;
 	}
 	
 	public void insert(int value) {
 		
+		if(!isHeapFull()) {
+			++size;
+			++currentIndex;
+			data[currentIndex] = value;
+		}
+		if(currentIndex != 1) {
+			checkParentAndSwap();
+		}
 	}
-	
+
 	public void delete(int value) {
 		
+	}
+	
+	private void checkParentAndSwap() {
+		
+		for(int parentIndex = getParentIndex(currentIndex); parentIndex >= 0;) {
+			if(data[parentIndex] < data[currentIndex]) {
+
+				int temp = data[parentIndex];
+				data[parentIndex] = data[currentIndex];
+				data[currentIndex] = temp;
+				
+				parentIndex = getParentIndex(parentIndex);
+			}
+			else {
+				break;
+			}
+		}
+	}
+	
+	public boolean isHeapEmpty() {
+		return size == 0;
+	}
+	
+	public boolean isHeapFull() {
+		return size == capacity;
+	}
+	
+	public String viewHeap() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[ ");
+		
+		for(int i = 0; i < this.size; i++) {
+			builder.append(data[i] + " ");
+		}
+		builder.append(" ]");
+		return builder.toString();
+	}
+	
+	private int getParentIndex(int currentIndex) {
+		return ((currentIndex - 1)/2);
 	}
 }
