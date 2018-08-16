f61c5b3286fe8e5ef1c7101dfe674c5cb4d712a2
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2018-08-15 20:07:11 -0700

    Max Heap Implementation - Madanraj Venkatesan

--- src/com/learnings/datastructure/heap/App.java
+++ src/com/learnings/datastructure/heap/App.java
@@ -4,15 +4,49 @@ public class App {
 	
 	public static void main(String[] args) {
 		MaxHeap heap = new MaxHeap(10);
+
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
 		heap.insert(10);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
 		heap.insert(9);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
 		heap.insert(8);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
 		heap.insert(7);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
 		heap.insert(6);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
 		heap.insert(5);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
 		heap.insert(4);
-		heap.insert(3);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
 		heap.insert(20);
-		System.out.println(heap.viewHeap());
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
+		heap.insert(1);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
+		heap.insert(2);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+
+		heap.delete();
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+
+		heap.delete();
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+
+		heap.delete();
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
+		
+		heap.insert(2);
+		System.out.println("Heap: " + heap.viewHeap() + " Size: " + heap.getSize() + "\n");
 	}
 }
--- src/com/learnings/datastructure/heap/MaxHeap.java
+++ src/com/learnings/datastructure/heap/MaxHeap.java
@@ -19,29 +19,72 @@ public class MaxHeap {
 	
 	public void insert(int value) {
 		
+		System.out.println("Adding " + value + " to the Heap.");
 		if(!isHeapFull()) {
-			++size;
-			++currentIndex;
-			data[currentIndex] = value;
+			data[++currentIndex] = value;
+			size++;
+			
+			if(currentIndex > 0) {
+				checkParentAndSwap(currentIndex);
+			}
 		}
-		if(currentIndex != 1) {
-			checkParentAndSwap();
+		else {
+			System.out.println("Heap is Full.");
 		}
 	}
 
-	public void delete(int value) {
+	public void delete() {
 		
+		System.out.println("Deleting " + data[0] + " from the Heap.");
+		if(!isHeapEmpty()) {
+
+			//Replace the Root Element with the Last Inserted Element
+			data[0] = data[currentIndex];
+			currentIndex--;
+			size--;
+			
+			if(currentIndex > 0) {
+				checkChildrenAndSwap(0);
+			}
+		}
 	}
 	
-	private void checkParentAndSwap() {
+	private void checkChildrenAndSwap(int index) {
 		
-		for(int parentIndex = getParentIndex(currentIndex); parentIndex >= 0;) {
-			if(data[parentIndex] < data[currentIndex]) {
+		if(hasChildNodes(index)) {
+			for(int parentIndex = index, childIndex = getMaxChildIndex(index); childIndex < size; ) {
+				
+				if(data[parentIndex] < data[childIndex]) {
+
+					int temp = data[parentIndex];
+					data[parentIndex] = data[childIndex];
+					data[childIndex] = temp;
+
+					parentIndex = childIndex;
+					childIndex = getMaxChildIndex(childIndex);
+				}
+				else {
+					break;
+				}
+			}
+		}
+	}
+
+	private boolean hasChildNodes(int index) {
+		return ((2 * index) + 1) <= size;
+	}
+
+	private void checkParentAndSwap(int index) {
+		
+		for(int parentIndex = getParentIndex(index), childIndex = index; parentIndex >= 0;) {
+			
+			if(data[parentIndex] < data[childIndex]) {
 
 				int temp = data[parentIndex];
-				data[parentIndex] = data[currentIndex];
-				data[currentIndex] = temp;
-				
+				data[parentIndex] = data[childIndex];
+				data[childIndex] = temp;
+
+				childIndex = parentIndex;
 				parentIndex = getParentIndex(parentIndex);
 			}
 			else {
@@ -65,11 +108,31 @@ public class MaxHeap {
 		for(int i = 0; i < this.size; i++) {
 			builder.append(data[i] + " ");
 		}
-		builder.append(" ]");
+		builder.append("]");
 		return builder.toString();
 	}
 	
-	private int getParentIndex(int currentIndex) {
-		return ((currentIndex - 1)/2);
+	private int getParentIndex(int index) {
+		return ((index - 1)/2);
+	}
+	
+	private int getMaxChildIndex(int index) {
+		
+		//Check whether the current Index has left and right Child nodes
+		int maxChildIndex;
+		int leftChildIndex = (2 * index) + 1;
+		int rightChildIndex = (2 * index) + 2;
+		
+		if(rightChildIndex < size) { //Assuming if it has right child it should also have left child
+			maxChildIndex = (data[leftChildIndex] > data[rightChildIndex]) ? leftChildIndex : rightChildIndex;
+		}
+		else {
+			maxChildIndex = leftChildIndex;
+		}
+		return maxChildIndex;
+	}
+
+	public int getSize() {
+		return size;
 	}
 }
