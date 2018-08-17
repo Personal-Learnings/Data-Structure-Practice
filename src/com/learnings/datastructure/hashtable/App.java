f4fdefaaf8a5b52da32f4b05734d83272c6f783e
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2018-08-16 22:55:13 -0700

    Hash Table Implementation - Madanraj Venkatesan

--- src/com/learnings/datastructure/hashtable/App.java
+++ src/com/learnings/datastructure/hashtable/App.java
@@ -4,12 +4,27 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		HashTable hashTable = new HashTable(4);
+		HashTable hashTable = new HashTable(11);
 		hashTable.insert("India");
 		hashTable.insert("UK");
 		hashTable.insert("Dubai");
 		hashTable.insert("USA");
 		hashTable.insert("Australia");
+		hashTable.insert("Dubai");
+		
+		System.out.println();
+		hashTable.getSize();
 		hashTable.viewHashTable();
+
+		System.out.println();
+		hashTable.find("Dubai");
+		
+		System.out.println();
+		hashTable.delete("Dubai");
+		hashTable.getSize();
+		hashTable.viewHashTable();
+		
+		System.out.println();
+		hashTable.find("Dubai");
 	}
 }
--- src/com/learnings/datastructure/hashtable/HashTable.java
+++ src/com/learnings/datastructure/hashtable/HashTable.java
@@ -12,13 +12,145 @@ public class HashTable {
 		
 		if(!isPrimeNumber(capacity)) {
 			this.capacity = getNextPrimeNumber(capacity);
-			System.out.println("Since " + capacity + " is not a prime number, so increasing the capacity to the next prime number " + this.capacity);
+			System.out.println("Since " + capacity + " is not a prime number, so the capacity is autoincremented to the next prime number " + this.capacity);
 		}
 		else {
 			this.capacity = capacity;
 		}
 		this.data = new Object[this.capacity];
 		this.size = 0;
+	}
+	
+	public void insert(Object element) {
+		if(!isHashTableFull()) {
+			
+			int index = hash(element);
+			
+			if(data[index] == null) {
+				data[index] = element;
+				size++;
+				System.out.println("Inserting " + element + " at Position " + index);
+			}
+			else {
+				while(true) {
+					index = (index + doubleHash(element)) % capacity;
+					if(data[index] == null) {
+						data[index] = element;
+						size++;
+						System.out.println("Inserting " + element + " at Position " + index);
+						return;
+					}
+				}
+			}
+		}
+		else {
+			System.out.println("Hash Table is Full !!!");
+		}
+	}
+	
+	public Object find(Object element) {
+		
+		Object newElement = null;
+		if(!isHashTableEmpty()) {
+			
+			int index = hash(element);
+			
+			if(data[index] != null && data[index].equals(element)) {
+				System.out.println("Found " + element + " at Index " + index);
+				newElement = data[index];
+			}
+			else {
+				int i = 1;
+				while(true) {
+					index = (index + doubleHash(element)) % capacity;
+					
+					if(data[index] != null && data[index].equals(element)) {
+						newElement = data[index];
+						System.out.println("Found " + element + " at Index " + index);
+						break;
+					}
+					
+					//To Break the Loop if the Element is not found in the Hash Table
+					if(i ==  capacity) {
+						System.out.println("Element " + element + " not Found in the Hash Table !!!");
+						break;
+					}
+					++i;
+				}
+			}
+		}
+		else {
+			System.out.println("HashTable is Empty !!!");
+		}
+		return newElement;
+	}
+	
+	public void delete(Object element) {
+		
+		if(!isHashTableEmpty()) {
+			
+			int index = hash(element);
+			
+			if(data[index] != null && data[index].equals(element)) {
+				data[index] = null;
+				size--;
+				System.out.println("Deleting " + element + " at Index " + index);
+			}
+			else {
+				int i = 1;
+				while(true) {
+					index = (index + doubleHash(element)) % capacity;
+					if(data[index] != null && data[index].equals(element)) {
+						data[index] = null;
+						size--;
+						System.out.println("Deleting " + element + " at Index " + index);
+						break;
+					}
+					
+					//To Break the Loop if the Element is not found in the Hash Table
+					if(i ==  capacity) {
+						System.out.println("Element " + element + " not Found in the Hash Table !!!");
+						break;
+					}
+					++i;
+				}
+			}
+		}
+		else {
+			System.out.println("HashTable is Empty !!!");
+		}
+	}
+	
+	public int getSize() {
+		System.out.println("Hash Table Size: " + size);
+		return size;
+	}
+	
+	public boolean isHashTableFull() {
+		return size == capacity;
+	}
+	
+	public boolean isHashTableEmpty() {
+		return size == 0;
+	}
+	
+	public void viewHashTable() {
+		System.out.print("[ ");
+		for(int i = 0; i < capacity; i++) {
+			System.out.print(data[i] + "  ");
+		}
+		System.out.println("]");
+	}
+	
+	private int hash(Object element) {
+		int hashCode = Math.abs(element.hashCode() % capacity);
+		return hashCode;
+	}
+	
+	private int doubleHash(Object element) {
+		int hashCode = Math.abs(element.hashCode() % capacity);
+		hashCode = 3 - (hashCode % 3);
+		return (hashCode == 0) ? hashCode += capacity : Math.abs(hashCode);
 	}
 	
 	private int getNextPrimeNumber(int number) {
@@ -36,87 +168,5 @@ public class HashTable {
 	            return false;
 	    }
 	    return true;
-	}
-
-	private int hash(Object element) {
-		int hashCode = Math.abs(element.hashCode() % capacity);
-		return hashCode;
-	}
-	
-	private int doubleHash(Object element) {
-		int hashCode = Math.abs(element.hashCode() % capacity);
-		hashCode = 3 - (hashCode % 3);
-		return (hashCode == 0) ? hashCode += capacity : Math.abs(hashCode);
-	}
-	
-	public void insert(Object element) {
-		if(!isHashTableFull()) {
-			int index = hash(element);
-			
-			if(data[index] == null) {
-				data[index] = element;
-				size++;
-				System.out.println("Inserting the Object at Position " + index);
-			}
-			else {
-				while(true) {
-					index = (index + doubleHash(element)) % capacity;
-					if(data[index] == null) {
-						data[index] = element;
-						size++;
-						System.out.println("Inserting the Object at Position " + index);
-						return;
-					}
-				}
-			}
-		}
-		else {
-			System.out.println("Hash Table Full !!!");
-		}
-	}
-	
-	public Object find(Object element) {
-		Object newElement = null;
-		if(!isHashTableEmpty()) {
-			
-			int index = hash(element);
-			
-			if(data[index] != null && data[index].equals(element)) {
-				newElement = data[index];
-			}
-			else {
-				while(true) {
-					index = (index + doubleHash(element)) % capacity;
-					if(data[index] != null) {
-						newElement = data[index];
-						return;
-					}
-				}
-			}
-		}
-		else {
-			System.out.println("HashTable Empty.");
-		}
-		return newElement;
-	}
-
-	public int getSize() {
-		return size;
-	}
-	
-	public boolean isHashTableFull() {
-		return size == capacity;
-	}
-	
-	public boolean isHashTableEmpty() {
-		return size == 0;
-	}
-
-	public void viewHashTable() {
-		System.out.print("[ ");
-		for(int i = 0; i < capacity; i++) {
-			System.out.print(data[i] + "  ");
-		}
-		System.out.println("]");
 	}
 }
